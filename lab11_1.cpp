dabd0d15d2ac2988bb1a0cd8e5b4a0bc55f786e2
ErrkkSkyseer <lolplayer78876@gmail.com>
2024-12-20 15:11:20 +0700

    Lab11 - Completed!

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,40 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string randGrade()
+{
+    srand(time(0));
+    int r = rand() % 9;
+    switch (r)
+    {
+    case 0: return "A"  ; break;
+    case 1: return "B+" ; break;
+    case 2: return "B"  ; break;
+    case 3: return "C+" ; break;
+    case 4: return "C"  ; break;
+    case 5: return "D+" ; break;
+    case 6: return "D"  ; break;
+    case 7: return "F"  ; break;
+    case 8: return "W"  ; break;
+    }
+    return " ";
+}
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    string t;
+    getline(cin,t);
+    getline(cin,t);
+    getline(cin,t);
+    cout << "You will get " << randGrade() << " in this 261102.";
+}
+
+
+
+
+
+
+
